9614348baac8dfdef729a04524d28ada6ccd36f5
chaesar09 <43412730+chaesar09@users.noreply.github.com>
2025-07-09 15:21:33 +0700

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using Launcher;
+using System.Net;
+
+namespace WPFS4Launcher
+{
+    internal class Constants
+    {
+        public static bool KRClient = false;
+
+        public static MainWindow LoginWindow;
+        public static IPEndPoint ConnectEndPoint = new IPEndPoint(IPAddress.Parse("156.67.219.144"), 28001);
+    }
+}
